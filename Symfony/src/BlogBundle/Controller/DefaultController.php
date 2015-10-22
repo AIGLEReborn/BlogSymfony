b059c9533fa52e543875c6ee984dc459d768c7fa
Baptiste Leulliette <bleulliette@hotmail.fr>
2015-10-22 16:03:36 +0200

    Creation des routes et méthodes associées.

--- Symfony/src/BlogBundle/Controller/DefaultController.php
+++ Symfony/src/BlogBundle/Controller/DefaultController.php
@@ -11,5 +11,23 @@ class DefaultController extends Controller
         return $this->render('BlogBundle:Default:index.html.twig', array('name' => "COUCOU LES REBORN"));
     }
 
-    public function 
+    public function displayPostAction($id) {
+    	//TODO display post
+    	return $this->render('BlogBundle:Default:index.html.twig', array('name' => "COUCOU LES REBORN"));
+    }
+
+    public function addPostAction() {
+    	//Todo Form et insert
+    	return $this->render('BlogBundle:Default:index.html.twig', array('name' => "COUCOU LES REBORN"));
+    }
+
+    public function editPostAction($id) {
+    	//Todo affichage form, remplissage et update
+    	return $this->render('BlogBundle:Default:index.html.twig', array('name' => "COUCOU LES REBORN"));
+    }
+
+    public function deletePost($id) {
+    	//Todo delete post
+    	return $this->render('BlogBundle:Default:index.html.twig', array('name' => "COUCOU LES REBORN"));
+    }
 }
