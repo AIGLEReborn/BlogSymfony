808b3f7e6cec8b64f922a31aad9606e0f64b3e6b
Baptiste Leulliette <bleulliette@hotmail.fr>
2015-10-22 18:04:58 +0200

    Oubli insertions BDD

--- bddblog.sql
+++ bddblog.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Jeu 22 Octobre 2015 à 17:34
+-- Généré le :  Jeu 22 Octobre 2015 à 18:04
 -- Version du serveur :  5.6.17
 -- Version de PHP :  5.5.12
 
@@ -33,7 +33,15 @@ CREATE TABLE IF NOT EXISTS `post` (
   `contenu` longtext COLLATE utf8_unicode_ci NOT NULL,
   `datePublication` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3 ;
+
+--
+-- Contenu de la table `post`
+--
+
+INSERT INTO `post` (`id`, `titre`, `url`, `contenu`, `datePublication`) VALUES
+(1, 'test 1', 'test-1', 'contenu du test 1', '2015-10-22 00:00:00'),
+(2, 'test2', 'test-2', 'contenu du test 2', '2015-10-22 00:00:00');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
